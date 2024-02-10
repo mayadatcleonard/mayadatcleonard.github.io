a393ba6ebc3067d0de019ad7706dffa1c471093e
mayadatcleonard <157981652+mayadatcleonard@users.noreply.github.com>
2024-02-10 11:37:15 -0800

    LAB1

---
+++ LAB1/story_content/user.js
@@ -0,0 +1,35 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6FWUqghm6Fr":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  const target = object('6PuKmq6reKz');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5YeFJxsINao';
+const growAmount = 0.2;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: `${1 + growAmount}` }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
