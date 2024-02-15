7ad36217446b679b98fc736d2ef8f8425ce6c3d1
mayadatcleonard <157981652+mayadatcleonard@users.noreply.github.com>
2024-02-15 14:15:14 -0800

    lab2

---
+++ Lab2/story_content/6Zv2mLkrkE9_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.84.31722.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:04.329%0D%0AWelcome%20To%20Tutorial%202024%0D%0AHow%20To%20Know%20\u0026%20Build%20My%20Family%0D%0A%0D%0A00:00:04.479%20--%3E%2000:00:04.885%0D%0ATree%0D%0A%0D%0A"}')
